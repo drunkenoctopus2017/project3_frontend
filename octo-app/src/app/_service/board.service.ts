3e56ce4813e4d1bd265ca8187bff61cc27a83d1f
JunkyardStar <jibbgh@gmail.com>
2017-11-21 10:45:33 -0500

    backing up before merge

--- octo-app/src/app/_service/board.service.ts
+++ octo-app/src/app/_service/board.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@angular/core';
+import {Http, Response} from '@angular/http';
+import {SystemBoard} from '../_model/SystemBoard';
 
 @Injectable()
 export class BoardService {
 
-  constructor() { }
+  //reserved for simple initialization like wiring constructor params to properties
+  constructor(private boardService: BoardService, private http:Http) { }
 
+  //methods here 
+  createUpdateBoard(){
+    let url = "";
+    let body = {}
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
+  }
 }
--- octo-app/src/app/create-update-board/create-update-board.component.ts
+++ octo-app/src/app/create-update-board/create-update-board.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import {BoardService} from '../_service/board.service';
 
 @Component({
   selector: 'app-create-update-board',
   templateUrl: './create-update-board.component.html',
-  styleUrls: ['./create-update-board.component.css']
+  styleUrls: ['./create-update-board.component.css'],
+  providers: [BoardService]
 })
 export class CreateUpdateBoardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private boardService: BoardService, private router: Router) { }
 
   ngOnInit() {
   }
 
+  submit(){
+    this.router.navigate(['/mainMenu']);
+  }
 }
