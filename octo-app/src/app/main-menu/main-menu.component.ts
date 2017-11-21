13302ca942fb1d87cabb6a61b06ee7f81dc5c528
waveeyang <waveeyang@gmail.com>
2017-11-21 08:51:28 -0500

    main menu looks like it did for project 2

--- octo-app/src/app/main-menu/main-menu.component.ts
+++ octo-app/src/app/main-menu/main-menu.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 import { SystemUser } from '../_model/SystemUser';
+import { SystemBoard } from '../_model/SystemBoard';
 
 @Component({
   selector: 'app-main-menu',
@@ -7,10 +10,58 @@ import { SystemUser } from '../_model/SystemUser';
   styleUrls: ['./main-menu.component.css']
 })
 export class MainMenuComponent implements OnInit {
-  user: SystemUser;
-  constructor() { }
+  user: SystemUser = {
+    id: 1,
+    username: "im",
+    password: "dabes",
+    firstName: "Cool",
+    lastName: "Guy",
+    role: 200
+  };
+  boards: SystemBoard[];
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    this.boards = [{
+      id: 1,
+      name: "Uno Board",
+      startDate: new Date(),
+      duration: 14
+    },
+    {
+      id: 2,
+      name: "Board Number 2",
+      startDate: new Date(),
+      duration: 15
+    }];
+  }
+
+  percentComplete(b: SystemBoard): string {
+    return 40+"%";
+  }
+
+  createScrumBoard() {
+    console.log("create scrum board method!");
+    this.router.navigate(['/createUpdateBoard']);
+  }
+
+  viewBoard(b: SystemBoard) {
+    console.log(b.name + "'s view scrum board method!");
+    this.router.navigate(['/boardStoryLanes']);
+  }
+
+  editScrumBoard(b: SystemBoard){
+    console.log(b.name + "'s edit scrum board method!");
+    this.router.navigate(['/createUpdateBoard']);
+  }
+
+  getAllUsers(b: SystemBoard){
+    console.log(b.name + "'s get users view method!");
+    this.router.navigate(['/assignMembers']);
+  }
+
+  triggerModal(b: SystemBoard){
+    console.log(b.name + "'s trigger delete board method!");
   }
 
 }
