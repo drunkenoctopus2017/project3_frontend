d353bbb160c4934740a71f80d39ae0e8a4759ea7
waveeyang <waveeyang@gmail.com>
2017-11-26 20:13:54 -0500

    base for create/update board frontend

--- octo-app/src/app/create-update-board/create-update-board.component.ts
+++ octo-app/src/app/create-update-board/create-update-board.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { BoardService } from '../_service/board.service';
 
 @Component({
   selector: 'app-create-update-board',
@@ -8,8 +9,13 @@ import { ActivatedRoute, Params } from '@angular/router';
 })
 export class CreateUpdateBoardComponent implements OnInit {
   boardID: number;
-  status: boolean;
-  constructor(private route: ActivatedRoute) { }
+  status: string;
+  startDate: string = this.formatDate(new Date());
+  //startDate: Date = new Date();
+  sbName: string;
+  duration: number;
+
+  constructor(private route: ActivatedRoute, private boardService: BoardService) { }
 
   ngOnInit() {
     this.route.params.forEach(
@@ -18,6 +24,37 @@ export class CreateUpdateBoardComponent implements OnInit {
         this.boardID = params["id"]; //grab the board ID
       }
     )
+    console.log("this is status => "+this.status);
+    if(this.status == "false"){ //if it's in edit mode 
+      console.log("editing");
+      let currBoard = this.boardService.getBoardByBoardID(this.boardID);
+      this.sbName = currBoard.name;
+      this.startDate = this.formatDate(new Date(currBoard.startDate));
+      //this.startDate = new Date(currBoard.startDate);
+      console.log(this.startDate);
+      this.duration = currBoard.duration;
+    }
+    console.log("done checking status");
   }
+
+
+  //debugging purposes
+  seeWhatValueItIs() {
+    console.log(this.startDate);
+  }
+
+  //this is needed for formatting the date to fill in the input field, 
+  //for some reason the input field won't display a raw date object as is
+  formatDate(date: Date): string {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+
+    return [year, month, day].join('-');
+}
 
 }
