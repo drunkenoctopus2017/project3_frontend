6191e8d76f4755e1e4b0a174b71b0af9f8be98b3
JunkyardStar <jibbgh@gmail.com>
2017-11-28 16:31:25 -0500

    just to be safe

--- octo-app/src/app/_service/task.service.ts
+++ octo-app/src/app/_service/task.service.ts
@@ -8,21 +8,9 @@ export class TaskService {
      
    }
    getTaskById(storyId: number){
-     return this.dummyData;
    }
 
-  dummyData: Task[] = [{
-    id: 1,
-    status: 2,
-    description: "this"
-  }, 
-  {
-    id: 2,
-    status: 3,
-    description: "that"
-  }
-  ];
-  
+
   private handleError(error: any): Promise<any> {
     console.error('An error occurred', error); // for demo purposes only
     return Promise.reject(error.message || error);
