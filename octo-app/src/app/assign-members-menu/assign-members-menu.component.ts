73257e019fe95e8d7dce8035adca763a639deefe
waveeyang <waveeyang@gmail.com>
2017-11-29 09:25:26 -0500

    added a back button to assign members view

--- octo-app/src/app/assign-members-menu/assign-members-menu.component.ts
+++ octo-app/src/app/assign-members-menu/assign-members-menu.component.ts
@@ -161,4 +161,8 @@ export class AssignMembersMenuComponent implements OnInit {
       console.log(response);
     });
   }
+
+  goBackToMainMenu() {
+    this.router.navigate(['/mainMenu']);
+  }
 }
