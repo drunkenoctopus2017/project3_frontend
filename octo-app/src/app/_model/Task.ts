50a1f2fe637c233a5004020ad7a05744097fffd6
waveeyang <waveeyang@gmail.com>
2017-11-30 15:08:02 -0500

    stuff

--- octo-app/src/app/_model/Task.ts
+++ octo-app/src/app/_model/Task.ts
@@ -2,4 +2,5 @@ export class Task{
     id: number;
     status: number;
     description: string;
+    storyId: number;
 }
--- octo-app/src/app/_service/task.service.ts
+++ octo-app/src/app/_service/task.service.ts
@@ -33,6 +33,11 @@ export class TaskService {
     return this.http.get(url).toPromise().then().catch(this.handleError);
   }
 
+  deleteTaskById(id: number){
+    const url = zuulUrl + "octo-task-service/deleteTaskById/" + id;
+    return this.http.get(url).toPromise().then().catch(this.handleError);
+  }
+
   private handleError(error: any): Promise<any> {
     console.error('An error occurred in task service: ', error); // for demo purposes only
     return Promise.reject(error.message || error);
